20d266c5b148c19b4df805de285f635bad392a1c
Cullan Luther <cullan.luther@gmail.com>
2016-07-20 01:32:18 -0500

    wrap images in container so border will work

--- app/contentScript.js
+++ app/contentScript.js
@@ -2,4 +2,8 @@
 chrome.runtime.sendMessage({ fn: "getSelector" } , function(response) {
 	//console.log(response.selector, response.style);
     $(response.selector).addClass(response.style);
+
+    if($('img.borderererzzz')){
+        $('img.borderererzzz').wrap( "<span class='img-shadow'></span>" );
+    }
 });
--- app/remove.js
+++ app/remove.js
@@ -1,2 +1,8 @@
+    //Remove image wrapper
+    var $imageWrapperz = $('img.borderererzzz');
 
+    if($imageWrapperz){
+    	console.log('true');
+        $imageWrapperz.unwrap('<span class="img-shadow"></span>');
+    }
     $('*').removeClass('borderererzzz shaderererzzz bothemememzzz');

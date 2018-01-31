5d17505e9cb2367bc1a141e05796c48b3e839a87
Cullan Luther <cullan.luther@gmail.com>
2018-01-30 20:56:50 -0600

    added sass and compass to project, some minor css updates are still needed

---
+++ app/config.rb
@@ -0,0 +1,17 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_path = "assets/stylesheets/css"
+sass_path = "assets/stylesheets/sass"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+
+preferred_syntax = :scss
